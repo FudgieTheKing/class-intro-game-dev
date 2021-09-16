c81c5f142f91e609557e853bf597ddb4577ebe66
FudgieTheKing <57367236+FudgieTheKing@users.noreply.github.com>
2021-09-16 10:27:07 -0400

    added Files OwO

---
+++ Assets/movementclouds.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementclouds : MonoBehaviour
+{
+    //set speed of cloud movement 
+    public float speed = 1f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get current game object postition
+        Vector3 temp = this.transform.position;
+        if (temp.x <= 32)
+        {
+            //add to the x of our current postion
+            temp.x += speed * Time.deltaTime;
+            //setting current postion to temp
+            this.transform.position = temp;
+        }
+        else
+        {
+            temp.x = -26f;
+            this.transform.position = temp;
+        }
+    }
+}
